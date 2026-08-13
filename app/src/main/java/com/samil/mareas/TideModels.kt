0c108a691af50abbcd57cc627b60a645bf01d3fb
ENERGEABC <energeabc1@gmail.com>
2026-08-13 09:33:38 +0200

    Create TideModels.kt

---
+++ app/src/main/java/com/samil/mareas/TideModels.kt
@@ -0,0 +1,25 @@
+package com.samil.mareas
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+data class TideEvent(
+      val dateTime: LocalDateTime,
+      val heightMeters: Double,
+      val isHighTide: Boolean
+  ) {
+      companion object {
+                private val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+
+                        fun parseDateTime(raw: String): LocalDateTime = LocalDateTime.parse(raw, FORMATTER)
+      }
+}
+
+data class TideState(
+      val previousEvent: TideEvent,
+      val nextEvent: TideEvent,
+      val progressFraction: Double,
+      val rising: Boolean,
+      val nextHighTide: TideEvent?,
+      val nextLowTide: TideEvent?
+    )
