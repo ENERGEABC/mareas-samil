766976f2307deab3e84ee2bc77d8c54f12426ab2
ENERGEABC <energeabc1@gmail.com>
2026-08-13 14:26:26 +0200

    Update TideModels.kt

--- app/src/main/java/com/samil/mareas/TideModels.kt
+++ app/src/main/java/com/samil/mareas/TideModels.kt
@@ -4,22 +4,20 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 data class TideEvent(
-      val dateTime: LocalDateTime,
-      val heightMeters: Double,
-      val isHighTide: Boolean
-  ) {
-      companion object {
-                private val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    val dateTime: LocalDateTime,
+    val heightMeters: Double,
+    val isHighTide: Boolean
+) {
+    companion object {
+        private val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
 
-                        fun parseDateTime(raw: String): LocalDateTime = LocalDateTime.parse(raw, FORMATTER)
-      }
+        fun parseDateTime(raw: String): LocalDateTime = LocalDateTime.parse(raw, FORMATTER)
+    }
 }
 
 data class TideState(
-      val previousEvent: TideEvent,
-      val nextEvent: TideEvent,
-      val progressFraction: Double,
-      val rising: Boolean,
-      val nextHighTide: TideEvent?,
-      val nextLowTide: TideEvent?
-    )
+    val previousEvent: TideEvent,
+    val nextEvent: TideEvent,
+    val progressFraction: Double,
+    val rising: Boolean
+)
